8216f590c2d45042cbe04d5dd16f0b15bb88d76d
Andrios Robert <andriosr@gmail.com>
2025-05-29 10:43:54 -0300

    Update dynamodb-scan.runbook.sh

--- dynamodb-scan.runbook.sh
+++ dynamodb-scan.runbook.sh
@@ -2,7 +2,7 @@
 
 # -------- Filters - optional --------
 FILTER_VALUE='{{ .filterValue | description "Enter attribute value" }}'
-FILTER_TYPE='{{ .filterType | type "select" | description "Type" | options "String" | default "String" }}'
+FILTER_TYPE='{{ .filterType | type "select" | description "Type" | options "String" "Number" "Binary" "Boolean" "Null" | default "String" }}'
 FILTER_CONDITION='{{ .filterCondition | type "select" | description "Condition" | options "Equal to" "Not equal to" "Less than or equal to" "Less than" "Greater than or equal to" "Greater than" "Between" "Exists" "Not exists" "Contains" "Not contains" "Begins with" | default "Equal to" }}'
 FILTER_ATTRIBUTE='{{ .filterAttribute | description "Attribute name" }}'
 
@@ -14,42 +14,66 @@ CMD="aws dynamodb scan --table-name Employee"
 
 # Add filter if provided
 if [ -n "$FILTER_ATTRIBUTE" ] && [ -n "$FILTER_VALUE" ]; then
+    # Build attribute value based on type
+    case "$FILTER_TYPE" in
+        "String")
+            ATTR_VALUE="{\"S\": \"$FILTER_VALUE\"}"
+            ;;
+        "Number")
+            ATTR_VALUE="{\"N\": \"$FILTER_VALUE\"}"
+            ;;
+        "Binary")
+            ATTR_VALUE="{\"B\": \"$FILTER_VALUE\"}"
+            ;;
+        "Boolean")
+            # Convert to lowercase for boolean
+            if [ "$FILTER_VALUE" == "true" ] || [ "$FILTER_VALUE" == "True" ] || [ "$FILTER_VALUE" == "TRUE" ]; then
+                ATTR_VALUE="{\"BOOL\": true}"
+            else
+                ATTR_VALUE="{\"BOOL\": false}"
+            fi
+            ;;
+        "Null")
+            ATTR_VALUE="{\"NULL\": true}"
+            ;;
+    esac
+    
     case "$FILTER_CONDITION" in
         "Equal to")
             CMD+=" --filter-expression \"#attr = :val\""
             CMD+=" --expression-attribute-names '{\"#attr\": \"$FILTER_ATTRIBUTE\"}'"
-            CMD+=" --expression-attribute-values '{\":val\": {\"S\": \"$FILTER_VALUE\"}}'"
+            CMD+=" --expression-attribute-values '{\":val\": $ATTR_VALUE}'"
             ;;
         "Not equal to")
             CMD+=" --filter-expression \"#attr <> :val\""
             CMD+=" --expression-attribute-names '{\"#attr\": \"$FILTER_ATTRIBUTE\"}'"
-            CMD+=" --expression-attribute-values '{\":val\": {\"S\": \"$FILTER_VALUE\"}}'"
+            CMD+=" --expression-attribute-values '{\":val\": $ATTR_VALUE}'"
             ;;
         "Less than")
             CMD+=" --filter-expression \"#attr < :val\""
             CMD+=" --expression-attribute-names '{\"#attr\": \"$FILTER_ATTRIBUTE\"}'"
-            CMD+=" --expression-attribute-values '{\":val\": {\"S\": \"$FILTER_VALUE\"}}'"
+            CMD+=" --expression-attribute-values '{\":val\": $ATTR_VALUE}'"
             ;;
         "Less than or equal to")
             CMD+=" --filter-expression \"#attr <= :val\""
             CMD+=" --expression-attribute-names '{\"#attr\": \"$FILTER_ATTRIBUTE\"}'"
-            CMD+=" --expression-attribute-values '{\":val\": {\"S\": \"$FILTER_VALUE\"}}'"
+            CMD+=" --expression-attribute-values '{\":val\": $ATTR_VALUE}'"
             ;;
         "Greater than")
             CMD+=" --filter-expression \"#attr > :val\""
             CMD+=" --expression-attribute-names '{\"#attr\": \"$FILTER_ATTRIBUTE\"}'"
-            CMD+=" --expression-attribute-values '{\":val\": {\"S\": \"$FILTER_VALUE\"}}'"
+            CMD+=" --expression-attribute-values '{\":val\": $ATTR_VALUE}'"
             ;;
         "Greater than or equal to")
             CMD+=" --filter-expression \"#attr >= :val\""
             CMD+=" --expression-attribute-names '{\"#attr\": \"$FILTER_ATTRIBUTE\"}'"
-            CMD+=" --expression-attribute-values '{\":val\": {\"S\": \"$FILTER_VALUE\"}}'"
+            CMD+=" --expression-attribute-values '{\":val\": $ATTR_VALUE}'"
             ;;
         "Between")
             # For Between, we'd need a second value field
             CMD+=" --filter-expression \"#attr BETWEEN :val1 AND :val2\""
             CMD+=" --expression-attribute-names '{\"#attr\": \"$FILTER_ATTRIBUTE\"}'"
-            CMD+=" --expression-attribute-values '{\":val1\": {\"S\": \"$FILTER_VALUE\"}, \":val2\": {\"S\": \"$FILTER_VALUE\"}}'"
+            CMD+=" --expression-attribute-values '{\":val1\": $ATTR_VALUE, \":val2\": $ATTR_VALUE}'"
             ;;
         "Exists")
             CMD+=" --filter-expression \"attribute_exists(#attr)\""
@@ -62,17 +86,17 @@ if [ -n "$FILTER_ATTRIBUTE" ] && [ -n "$FILTER_VALUE" ]; then
         "Contains")
             CMD+=" --filter-expression \"contains(#attr, :val)\""
             CMD+=" --expression-attribute-names '{\"#attr\": \"$FILTER_ATTRIBUTE\"}'"
-            CMD+=" --expression-attribute-values '{\":val\": {\"S\": \"$FILTER_VALUE\"}}'"
+            CMD+=" --expression-attribute-values '{\":val\": $ATTR_VALUE}'"
             ;;
         "Not contains")
             CMD+=" --filter-expression \"NOT contains(#attr, :val)\""
             CMD+=" --expression-attribute-names '{\"#attr\": \"$FILTER_ATTRIBUTE\"}'"
-            CMD+=" --expression-attribute-values '{\":val\": {\"S\": \"$FILTER_VALUE\"}}'"
+            CMD+=" --expression-attribute-values '{\":val\": $ATTR_VALUE}'"
             ;;
         "Begins with")
             CMD+=" --filter-expression \"begins_with(#attr, :val)\""
             CMD+=" --expression-attribute-names '{\"#attr\": \"$FILTER_ATTRIBUTE\"}'"
-            CMD+=" --expression-attribute-values '{\":val\": {\"S\": \"$FILTER_VALUE\"}}'"
+            CMD+=" --expression-attribute-values '{\":val\": $ATTR_VALUE}'"
             ;;
     esac
 fi
